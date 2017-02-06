fd7c9b4b03604598f94f5390fb13a85b5284eb5a
NINGHE ZHANG <ninghz2@cs.washington.edu>
2017-02-06 15:49:31 -0800

    Removed folder from repository

--- cse 143/hw 1/test.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class test { 
-   public static void main(String[] args) {
-    ArrayList<String> nameList = new ArrayList<String>("sam", "nina", "tina","jack");
-    AssassinManager manager = new AssassinManager(nameList);
-   }
-
-}
